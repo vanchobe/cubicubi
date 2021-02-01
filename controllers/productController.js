321a8cd3187deda07691714df456fd94c5c6528c
vanchobe <ivandm555@gmail.com>
2021-02-01 08:27:36 +0200

    add title of controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,11 +3,11 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Browse'});
 })
 
 router.get('/create', (req, res) => {
-    res.render('create')
+    res.render('create', {title: 'Create'})
 })
 
 module.exports = router;
